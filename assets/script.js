616bbd83e78635e85c55bd31e3c0e60cbd27f12c
AdilTehranli <adiljt@code.edu.az>
2023-06-02 11:44:44 +0400

    Updated

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+ 
+ function login(){
+let email='admin@gmail.com'
+    let pass='12345'
+
+    
+    let emailv = document.getElementById('emai').value;
+    let passv = document.getElementById('passw').value;
+    
+    (email==emailv && pass==passv )?
+        alert('correct'):
+    
+        alert('uncorrect')
+
+  
+}
+
+
+
+
